83990b48f0d50384385975c3b0b48c748d26b561
n3ryoficial <126675401+n3ryoficial@users.noreply.github.com>
2024-07-22 15:46:06 -0300

    Add files via upload

---
+++ Principio.c
@@ -0,0 +1,15 @@
+// alguns caso necesida da forma de concertar os erros do codigo e da maquina aque se refere 
+// corringindo bugs do consolhe e da propria linguagem 
+
+
+// o fato que podemos parar a repetição 
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    printf("\nsoma: %d\n", 10 + 20 );
+    printf("\nsubtracao: %d\n", 10 - 20);
+    printf("\nmultiplicacao: %d\n", 10 * 20);
+    printf("\ndivisao: %f\n", 10 / 20 );
+    return 0;
+}
